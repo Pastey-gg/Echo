7614d4141505ebe0bf8f38f2539c867dec0ac41e
EvieePy <29671945+EvieePy@users.noreply.github.com>
2026-05-17 10:34:21 +1000

    Update Schema and config.example

--- database/schema.sql
+++ database/schema.sql
@@ -1,16 +1,16 @@
 CREATE TABLE IF NOT EXISTS pastes (
-    id              VARCHAR(7)   PRIMARY KEY,
+    id              TEXT   PRIMARY KEY,
     created_at      TIMESTAMPTZ  NOT NULL DEFAULT now(),
     views           INT          NOT NULL DEFAULT 0,
     expires_at      TIMESTAMPTZ,
     remaining_views INT,
     hashed_password TEXT,
-    safety_token    VARCHAR(32)  NOT NULL UNIQUE
+    safety_token    TEXT  NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS files (
-    id              VARCHAR(7)   PRIMARY KEY,
-    paste_id        VARCHAR(7)   NOT NULL REFERENCES pastes(id) ON DELETE CASCADE,
+    id              TEXT   PRIMARY KEY,
+    paste_id        TEXT   NOT NULL REFERENCES pastes(id) ON DELETE CASCADE,
     name            TEXT,
     language        TEXT,
     content         TEXT         NOT NULL,
